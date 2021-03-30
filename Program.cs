f4100030804efa2c2d8fb814df79993483e43611
Pedro Vila Luz <pedro.vila.luz@gmail.com>
2021-03-30 16:18:40 +0100

    "Acrescenet um printf"

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace Teste
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            int a=1;
+            System.Console.WriteLine(a);
+            int batata = a;
+            System.Console.WriteLine(batata);
         }
     }
 }
